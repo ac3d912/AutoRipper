2260cfebe0e857e9e2d9154922b1b936164fae0c
Cr0n1c <bhelms85@gmail.com>
2013-09-10 17:12:30 -0400

    Update autoripper_config.py

--- autoripper_config.py
+++ autoripper_config.py
@@ -1,11 +1,11 @@
-
 import os
+
+##########################[ MODIFY THESE SETTINGS TO FIT YOUR SETUP ]####################################
 
 #Variables that will reference binaries
 MAKEMKVCON = '/usr/bin/makemkvcon'         #this was tested with 1.8.4 beta
 HANDBRAKE_CLI = '/usr/bin/HandBrakeCLI'    #tested with 0.9.9
 
-list_of_bins = [ MAKEMKVCON, HANDBRAKE_CLI]
 
 #Variables that will reference Storage Locations
 RIP_LOCATION = r'/home/mediacenter/rips/'
@@ -13,15 +13,17 @@ CONVERT_LOCATION = r'/home/mediacenter/converts/'
 OUTPUT_MOVIE_LOCATION = r'/home/mediacenter/exthdd1/Movies/'
 MOVIE_LOCATION = [ OUTPUT_MOVIE_LOCATION ]    #This way if you have multiple locations it will check them all
 
-list_of_dirs = [ RIP_LOCATION, CONVERT_LOCATION, OUTPUT_MOVIE_LOCATION]
 
 #Variables that will reference hardware
 BLURAY_DEVICE = r'/dev/sr0'                   #This should be the default location for your bluray device
 MAKEMKV_DISC_NUM = BLURAY_DEVICE[-1]          #This is the cdrom drive number
 
-list_of_devs = [ BLURAY_DEVICE]
 
-#Need to put error checking to make sure directories and files exist
+###############################[ DON'T EDIT ANYTHING BELOW THIS ]#########################################
+
+list_of_bins = [ MAKEMKVCON, HANDBRAKE_CLI ]
+list_of_dirs = [ RIP_LOCATION, CONVERT_LOCATION, OUTPUT_MOVIE_LOCATION ]
+list_of_devs = [ BLURAY_DEVICE ]
 
 def dirExist(listOfDirectories):
     for directory in ListOfDirectories:
